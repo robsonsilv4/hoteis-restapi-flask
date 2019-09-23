a65493dac219aa5d80d0237631f189eeb21f48c6
robsonsilv4 <robsonsilv410@gmail.com>
2019-09-23 13:56:26 -0300

    Recursos de get e post

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 hoteis = [
     {
@@ -28,3 +28,39 @@ hoteis = [
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
+
+
+class Hotel(Resource):
+    def get(self, hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+
+        return {'message': 'Hotel não encontrado.'}, 404
+
+    def post(self, hotel_id):
+        argumentos = reqparse.RequestParser()
+        argumentos.add_argument('nome')
+        argumentos.add_argument('estrelas')
+        argumentos.add_argument('diaria')
+        argumentos.add_argument('cidade')
+
+        dados = argumentos.parse_args()
+
+        novo_hotel = {
+            'hotel_id': hotel_id,
+            'nome': dados['nome'],
+            'estrelas': dados['estrelas'],
+            'diaria': dados['diaria'],
+            'cidade': dados['cidade'],
+        }
+
+        hoteis.append(novo_hotel)
+
+        return novo_hotel, 200
+
+    def put(self):
+        pass
+
+    def delete(self):
+        pass
