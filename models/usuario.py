c7ba83fdb4819b95102f486b6c95d21f5bdceccb
robsonsilv4 <robsonsilv410@gmail.com>
2019-09-28 16:27:56 -0300

    Model de usuario

---
+++ models/usuario.py
@@ -0,0 +1,33 @@
+from sql_alchemy import banco
+
+
+class UsuarioModel(banco.Model):
+    __tablename__ = 'usuarios'
+    usuario_id = banco.Column(banco.Integer, primary_key=True)
+    email = banco.Column(banco.String(40))
+    senha = banco.Column(banco.String(40))
+
+    def __init__(self, hotel_id, email, senha):
+        self.email = email
+        self.senha = senha
+
+    @classmethod
+    def encontrar_usuario(cls, usuario_id):
+        usuario = cls.query.filter_by(usuario_id=usuario_id).first()
+        if usuario:
+            return usuario
+        return None
+
+    def salvar_hotel(self):
+        banco.session.add(self)
+        banco.session.commit()
+
+    def remover_usuario(self):
+        banco.session.delete(self)
+        banco.session.commit()
+
+    def json(self):
+        return {
+            'usuario_id': self.usuario_id,
+            'email': self.email,
+        }
