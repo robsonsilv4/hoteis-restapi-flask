5b870c82ddb1f06249a64e7917867400f9fb27f7
robsonsilv4 <robsonsilv410@gmail.com>
2019-09-29 22:28:09 -0300

    Login com jwt

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_restful import Api
 
 from resources.hotel import Hoteis, Hotel
-from resources.usuario import Usuario, UsuarioCadastro
+from resources.usuario import Usuario, UsuarioCadastro, UsuarioLogin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'super_secreta'
 api = Api(app)
+jwt = JWTManager(app)
 
 
 @app.before_first_request
@@ -19,6 +22,7 @@ api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<int:hotel_id>')
 api.add_resource(Usuario, '/usuarios/<int:usuario_id>')
 api.add_resource(UsuarioCadastro, '/cadastro')
+api.add_resource(UsuarioLogin, '/login')
 
 if __name__ == "__main__":
     from sql_alchemy import banco
--- resources/usuario.py
+++ resources/usuario.py
@@ -1,21 +1,35 @@
+from flask_jwt_extended import create_access_token
 from flask_restful import Resource, reqparse
+from werkzeug.security import safe_str_cmp
 
 from models.usuario import UsuarioModel
+
+atributos = reqparse.RequestParser()
+atributos.add_argument('email', type=str, required=True,
+                       help='O campo de email é obrigatório.')
+atributos.add_argument('senha', type=str, required=True,
+                       help='O campo de senha é obrigatório.')
 
 
 class UsuarioCadastro(Resource):
     def post(self):
-        atributos = reqparse.RequestParser()
-        atributos.add_argument(
-            'email', type=str, required=True, help='O campo de email é obrigatório.')
-        atributos.add_argument(
-            'senha', type=str, required=True, help='O campo de senha é obrigatório.')
         dados = atributos.parse_args()
         if UsuarioModel.encontrar_por_email(dados['email']):
             return {'message': 'O email {} já está sendo utilizado.'.format(dados['email'])}
         usuario = UsuarioModel(**dados)
         usuario.salvar_usuario()
         return {'message': 'Usuário criado com sucesso.'}, 201
+
+
+class UsuarioLogin(Resource):
+    @classmethod
+    def post(cls):
+        dados = atributos.parse_args()
+        usuario = UsuarioModel.encontrar_por_email(dados['email'])
+        if usuario and safe_str_cmp(usuario.senha, dados['senha']):
+            token_de_acesso = create_access_token(identity=usuario.usuario_id)
+            return {'access_token': token_de_acesso}, 200
+        return {'message': 'O email ou senha estão incorretos.'}, 401
 
 
 class Usuario(Resource):
